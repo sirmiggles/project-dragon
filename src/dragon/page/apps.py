f3cc343de083528455ba24c222ed4b571717265c
TinBryn <griffk04@gmail.com>
2019-08-13 07:35:45 +0800

    Added an item model

--- src/dragon/page/apps.py
+++ src/dragon/page/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
 
-class PollsConfig(AppConfig):
+class PageConfig(AppConfig):
+    """docstring"""
     name = 'page'
--- src/dragon/page/models.py
+++ src/dragon/page/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Item(models.Model):
+    id = models.ForeignKey
+    title = models.CharField(max_length=200)
+    published_date = models.DateField
+    type = models.CharField(max_length=200)
+    remarks = models.CharField(max_length=200)
