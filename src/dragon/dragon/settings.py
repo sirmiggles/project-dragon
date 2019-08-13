d565b35d55874fa92382c35e3c350e9a8d455091
kieran <griffk04@gmail.com>
2019-08-13 09:00:32 +0800

    installed page app

--- src/dragon/dragon/settings.py
+++ src/dragon/dragon/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'page.apps.PageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- src/dragon/page/apps.py
+++ src/dragon/page/apps.py
@@ -2,5 +2,4 @@ from django.apps import AppConfig
 
 
 class PageConfig(AppConfig):
-    """docstring"""
     name = 'page'
