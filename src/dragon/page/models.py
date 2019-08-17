2e57ea7db57fb99ce572368ad45beeed726597e1
TinBryn <griffk04@gmail.com>
2019-08-17 10:12:33 +0800

    Added example tests and adjusted models to have tests work

--- src/dragon/page/models.py
+++ src/dragon/page/models.py
@@ -5,7 +5,22 @@ from django.db import models
 
 class Item(models.Model):
     id = models.ForeignKey
-    title = models.CharField(max_length=200)
-    published_date = models.DateField
-    type = models.CharField(max_length=200)
     remarks = models.CharField(max_length=200)
+
+    @property
+    def is_a_book(self):
+        return False
+
+class Published(Item):
+    published_date = models.DateField
+    publisher = models.CharField(max_length=200)
+
+
+class Book(Published):
+    isbn = models.IntegerField()
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+
+    @property
+    def is_a_book(self):
+        return True
--- src/dragon/page/tests.py
+++ src/dragon/page/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
+from .models import Item, Book
+
 # Create your tests here.
+
+class ItemTests(TestCase):
+
+    def test_books_should_say_they_are_a_book(self):
+        non_book_item = Item()
+        book_item = Book()
+        self.assertIs(book_item.is_a_book(), True)
+        self.assertIs(non_book_item.is_a_book, False)
