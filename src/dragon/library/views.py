604854f303cbcf4cf840db598399eb93dbecff07
celia233 <50320136+celia233@users.noreply.github.com>
2019-09-11 23:42:29 +0800

    Update views.py

--- src/dragon/library/views.py
+++ src/dragon/library/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 from .models import Book
 
@@ -29,3 +32,33 @@ def remove_book(request: HttpRequest, book_id: int) -> HttpResponse:
     book = get_object_or_404(Book, pk=book_id)
     book.delete()
     return HttpResponseRedirect('/library/')
+
+def booksearch(request):
+    template = "library\templates\library\library.html"
+    query = request.Get.get('q')
+    if query:
+        results = Book.objects.filter(Q(BookName__icontains=query) | Q(ISBN__icontains=query))
+    else:
+        results = Book.objects.all()
+    
+    paginator = paginator(request,results,num=10)
+    page = request.Get.get('page')
+    
+    try:
+        items = paginator.page(page)
+    except PageNotAnInteger:
+        items = paginator.page(1)
+    except EmptyPage:
+        items = paginator.page(paginator.num_pages)
+    
+    index = items.number - 1
+    max_index = len(paginator.page_range)
+    start_index = index -7 if index >= 7 else 0
+    end_index = index + 7 if index <= max_index - 7 else max_index
+    page_range = paginator.page_range[start_index:end_index]
+    
+    context = {
+        'items'= items,
+        'page_range':page_range,
+    }
+    return render(request, template, context)
