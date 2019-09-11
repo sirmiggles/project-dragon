3bf09ebece522f645c4ca203a1c27d6531e9f13d
TinBryn <griffk04@gmail.com>
2019-09-12 07:35:53 +0800

    Moved template and static folders

--- src/dragon/dragon/settings.py
+++ src/dragon/dragon/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'dragon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'dragon/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,8 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/dragon/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'dragon/static')
 ]
