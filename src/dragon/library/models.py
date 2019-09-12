c2d4cc1d0468577e8ab0885995c2858c94de8891
TinBryn <griffk04@gmail.com>
2019-09-12 08:48:45 +0800

    Added User model

--- src/dragon/library/models.py
+++ src/dragon/library/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
 
+class User(models.Model):
+    username = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.username
+
+
 class Tag(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 
 class Item(models.Model):
