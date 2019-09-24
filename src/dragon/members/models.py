c6a7f2d2d4d290e8967930bdcf72f89fdafcc9f9
laurent xhonneux <laurentxhonneux@msn.com>
2019-09-24 14:19:22 +0800

    Updated Members Model with range of new fields

--- src/dragon/members/models.py
+++ src/dragon/members/models.py
@@ -1,8 +1,29 @@
-from django.db import models
+from django.db.models import Model, CharField, AutoField, BooleanField, IntegerField, EmailField, DateField, TextField
+import django.utils
+from datetime import datetime
 
 
-class User(models.Model):
-    username = models.CharField(max_length=200)
+class User(Model):
+    Id = AutoField(primary_key=True, default=0)
+    FirstName = CharField(max_length=50, default='')
+    Surname = CharField(max_length=50, default='')
+    Email = EmailField(default='')
+    PhoneNumber = CharField(max_length=20, default='')
 
     def __str__(self):
         return self.username
+
+
+class ClubMember(User):
+
+    PreferredName = CharField(max_length=50, default='')
+    PreferredPronoun = CharField(max_length=20, default='')
+    GuildMember = BooleanField(default=False)
+    IsStudent = BooleanField(default=True)
+    UniversityID = CharField(max_length=8, default='')
+    JoinDate = DateField(default=django.utils.timezone.now)
+    Incidents = TextField(max_length=400,default='N/A')
+
+    rank_choices = ((0, 'Regular Member'), (1, 'Gatekeeper'), (2, 'Committee Member'))
+    ClubRank = IntegerField(choices=rank_choices, default=0)
+
