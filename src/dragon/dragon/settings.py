a76e2321139da080fd4c5c2da5a24f3dc8e95a3b
kieran <griffk04@gmail.com>
2019-09-25 13:50:14 +0800

    secret key can be loaded from environment variables

--- src/dragon/dragon/settings.py
+++ src/dragon/dragon/settings.py
@@ -19,7 +19,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$rbe#s_2)bih1pyp*$bwx%du#!b($(bugv!g@92au9@!hia&e$'
+
+SECRET_KEY = os.environ.get('DRAGON_SECRET_KEY')
+if SECRET_KEY == None:
+    print("warning: no secret key found, using public default")
+    SECRET_KEY = '$rbe#s_2)bih1pyp*$bwx%du#!b($(bugv!g@92au9@!hia&e$'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
