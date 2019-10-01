9b62dac69fa45602b1f4da9096b6e8cff91f1908
TinBryn <griffk04@gmail.com>
2019-10-01 11:38:18 +0800

    Added comments and docstring to various parts of the code trying to explain why things are done and suggesting future actions to take

--- src/dragon/library/apps.py
+++ src/dragon/library/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
 
-
+# In order to use an app within the project
+# it needs to be packaged into a name, this must
+# then be added to the list of apps in `settings.py`
 class LibraryConfig(AppConfig):
     name = 'library'
