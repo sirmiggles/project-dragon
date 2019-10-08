02c228b3604da8c8f4706ab2b17b902f7a494ae7
laurent xhonneux <laurentxhonneux@msn.com>
2019-10-08 13:43:54 +0800

    Added the new card game fields to forms.py

--- src/dragon/library/forms.py
+++ src/dragon/library/forms.py
@@ -59,6 +59,7 @@ class GameForm(forms.ModelForm):
             'condition': 'Condition'
         }
 
+
 class CardForm(forms.ModelForm):
     class Meta:
         model = Card
@@ -66,6 +67,12 @@ class CardForm(forms.ModelForm):
             'name',
             'description',
             'notes',
+            'minplayers',
+            'maxplayers',
+            'mingamelength',
+            'maxgamelength',
+            'difficulty',
+            'genre',
             'condition',
             'deck_type'
         ]
@@ -73,6 +80,11 @@ class CardForm(forms.ModelForm):
             'name': 'Name',
             'description': 'Description',
             'notes': 'Extra Notes',
+            'minplayers': 'Min. Players',
+            'maxplayers': 'Max. Players',
+            'mingamelength': 'Min. Game Length (minutes)',
+            'maxgamelength': 'Max. Game Length (minutes)',
+            'difficulty': 'Difficulty',
             'condition': 'Condition',
             'deck_type': 'Deck Type'
         }
