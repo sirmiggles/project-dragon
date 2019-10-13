b2484ec6fce99d998388dd4d8ddacc5336f55d82
TinBryn <griffk04@gmail.com>
2019-10-13 10:27:17 +0800

    Removed add_book test as it wasn't testable before, and merging them has just made the situation worse.

--- src/dragon/library/tests.py
+++ src/dragon/library/tests.py
@@ -11,18 +11,6 @@ class BookTests(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
 
-    def test_adding_a_book_with_no_title_should_fail(self):
-        """
-        It should not be possible to add a book without a title
-        so attempting to do so should fail and report the error, as the
-        view currently doesn't have any introspection it should be refactored
-        to be made more testable before this test can evaluate it
-
-        todo: make view testable and complete test
-        """
-        request = self.factory.post('library/add_book', data={'name': ''})
-        response = views.add_book(request)
-
     def test_is_available_method_on_book(self):
         # The fields for available will be removed so in the future
         # this test will need to borrow the book to become unavailable
